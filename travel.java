9ae5f796976e56f0cbbfa5af1131ef47072458a1
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-07-19 12:46:30 -0400

    Add files via upload

---
+++ travel.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class travel {
+	
+	
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
